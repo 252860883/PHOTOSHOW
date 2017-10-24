f213852633af73b7c192d5e7e623c1d43d881c8e
duhonghui <duhhxfs@xinfushe.com>
2017-10-24 09:52:41 +0800

    添加侧滑 喜欢功能

--- src/components/side.vue
+++ src/components/side.vue
@@ -7,58 +7,52 @@
 </template>
 
 <script>
-export default{
-    data(){
-        return{
-
-        }
-    },
-    methods:{
-        toLike(){
-            this.$router.push({path:'/like'})
-        }
+export default {
+  data() {
+    return {};
+  },
+  methods: {
+    toLike() {
+      this.$router.push({ path: "/like" });
     }
-}
-    
+  }
+};
 </script>
 
 <style>
-.info_content{
-    width: 15em;
-    height: 100%;
-    background: #000;
-    position: fixed;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    z-index: 1000;
-    color:#fff;
+.info_content {
+  width: 15em;
+  height: 100%;
+  background: #000;
+  position: fixed;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  z-index: 1000;
+  color: #fff;
 }
-.info_photo{
-    height: 5em;
-    width: 5em;
-    display: block;
-    background: #666;
-    border-radius: 50%;
-    flex: 0 1 5em;
-    line-height: 5em;
-    margin-top: 3em;
+.info_photo {
+  height: 5em;
+  width: 5em;
+  display: block;
+  background: #666;
+  border-radius: 50%;
+  flex: 0 1 5em;
+  line-height: 5em;
+  margin-top: 3em;
 }
-.info_content a{
-    height: 4em;
-    width: 100%;
-    line-height: 4em;
-    text-decoration: none;
-    color: #fff;
-    
+.info_content a {
+  height: 4em;
+  width: 100%;
+  line-height: 4em;
+  text-decoration: none;
+  color: #fff;
 }
-.info_content a:active{
-    background: #333;
+.info_content a:active {
+  background: #333;
 }
-.info_like{
-    margin-top: 3em;
+.info_like {
+  margin-top: 3em;
 }
-
-
 </style>
 
